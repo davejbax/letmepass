a28f1f9ad6af4450fb23f45a5fe5926ef469c779
davejbax <davejbax@gmail.com>
2018-04-16 23:06:06 +0100

    Add real password flags model

--- app/src/main/java/uk/co/davidbaxter/letmepass/presentation/CreationViewModel.java
+++ app/src/main/java/uk/co/davidbaxter/letmepass/presentation/CreationViewModel.java
@@ -6,6 +6,8 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 
+import uk.co.davidbaxter.letmepass.model.PasswordFlags;
+
 public class CreationViewModel extends ViewModel {
 
     // Bound fields: these are set by the view, and can be read/set by us
@@ -20,7 +22,7 @@ public class CreationViewModel extends ViewModel {
     public MutableLiveData<String> cloudLocation = new MutableLiveData<>();
     public MutableLiveData<String> deviceLocation = new MutableLiveData<>();
     public MutableLiveData<String> keyfileLocation = new MutableLiveData<>();
-    public MutableLiveData<DummyPasswordFlagsClass> passwordFlags = new MutableLiveData<>();
+    public MutableLiveData<PasswordFlags> passwordFlags = new MutableLiveData<>();
 
     public CreationViewModel() {
         this.cloudChecked.setValue(true);
@@ -52,72 +54,51 @@ public class CreationViewModel extends ViewModel {
 
     public void onMpChanged(CharSequence s, int start, int before, int count) {
         // TODO: temporary code here to give a prototype; actual code will call a Service
-        DummyPasswordFlagsClass pwdFlags = passwordFlags.getValue();
+        PasswordFlags pwdFlags = passwordFlags.getValue();
         if (pwdFlags == null)
-            pwdFlags = new DummyPasswordFlagsClass();
+            pwdFlags = new PasswordFlags();
 
-        pwdFlags.recalculate(s.toString());
+        generateFlags(pwdFlags, s.toString());
 
         // Re-set passwordFlags LiveData so UI updates
         passwordFlags.setValue(pwdFlags);
     }
 
-    public static class DummyPasswordFlagsClass {
-        public boolean goodLength,
-                hasSymbols,
-                hasMixedChars, // Contains uppercase, lowercase, and numbers
-                notBlacklisted;
+    public static void generateFlags(PasswordFlags flags, String password) {
+        // TODO: remove this temp function; will be implemented by a service in practice
+        // Working booleans from which to set the password flags
+        boolean numbers = false;
+        boolean upper = false;
+        boolean lower = false;
+        boolean symbols = false;
 
-        public DummyPasswordFlagsClass() {
+        // Iterate over characters to check whether containing certain chars
+        for (int i = 0; i < password.length(); i++) {
+            char c = password.charAt(i);
+            if ((c >= 32 && c <= 47)
+                    || (c >= 58 && c <= 64)
+                    || (c >= 91 && c <= 96)
+                    || (c >= 123 && c <= 126))
+                symbols = true;
+            if (c >= 97 && c <= 122)
+                lower = true;
+            if (c >= 65 && c <= 90)
+                upper = true;
+            if (c >= 48 && c <= 57)
+                numbers = true;
         }
 
-        public final void recalculate(String password) {
-            // Working booleans from which to set the password flags
-            boolean numbers = false;
-            boolean upper = false;
-            boolean lower = false;
-            boolean symbols = false;
+        // Set good length
+        flags.goodLength = password.length() >= 12;
 
-            // Iterate over characters to check whether containing certain chars
-            for (int i = 0; i < password.length(); i++) {
-                char c = password.charAt(i);
-                if ((c >= 32 && c <= 47)
-                        || (c >= 58 && c <= 64)
-                        || (c >= 91 && c <= 96)
-                        || (c >= 123 && c <= 126))
-                    symbols = true;
-                if (c >= 97 && c <= 122)
-                    lower = true;
-                if (c >= 65 && c <= 90)
-                    upper = true;
-                if (c >= 48 && c <= 57)
-                    numbers = true;
-            }
+        // Set mixed chars
+        flags.hasMixedChars = upper && lower && numbers;
 
-            // Set good length
-            this.goodLength = password.length() >= 12;
+        // Set symbols
+        flags.hasSymbols = symbols;
 
-            // Set mixed chars
-            this.hasMixedChars = upper && lower && numbers;
-
-            // Set symbols
-            this.hasSymbols = symbols;
-
-            // Set blacklisted
-            this.notBlacklisted = true;
-        }
-
-        @Override
-        public boolean equals(Object other) {
-            if (!(other instanceof DummyPasswordFlagsClass))
-                return false;
-
-            // We are only equal if we are the same type; compare all fields to ensure equality
-            DummyPasswordFlagsClass otherFlags = (DummyPasswordFlagsClass) other;
-            return otherFlags.goodLength == goodLength
-                    && otherFlags.hasSymbols == hasSymbols
-                    && otherFlags.hasMixedChars == hasMixedChars
-                    && otherFlags.notBlacklisted == notBlacklisted;
-        }
+        // Set blacklisted
+        flags.notBlacklisted = true;
     }
+
 }
